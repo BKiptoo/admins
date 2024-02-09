476ff0f2f5104a3e0066bfa5b999ef931dceed28
BKiptoo <bettsonk@gmail.com>
2024-02-09 09:18:49 +0300

    update

--- database/migrations/2024_02_08_144315_create_admins_table.php
+++ database/migrations/2024_02_08_144315_create_admins_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username')->nullable()->unique();
+            $table->string('phoneNumber');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
