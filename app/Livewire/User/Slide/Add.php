d90607e6283adbded51cd176fcd0f5e83b0bd973
BKiptoo <bettsonk@gmail.com>
2024-02-18 22:58:07 +0300

    Updated :arrows_counterclockwise:

---
+++ app/Livewire/User/Slide/Add.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User\Slide;
+
+use Livewire\Component;
+
+class Add extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.slide.add');
+    }
+}
